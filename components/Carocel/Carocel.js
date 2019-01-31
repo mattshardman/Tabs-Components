bcdf4ab1041a6f2788372295948e6e0d2d1d4e2b
mattshardman <mattshardman@gmail.com>
2019-01-31 16:00:23 +0000

    add carocel component

---
+++ components/Carocel/Carocel.js
@@ -0,0 +1,35 @@
+const boxes = document.querySelectorAll(".box");
+
+const leftBtn = document.querySelector("#move-left");
+const rightBtn = document.querySelector("#move-right");
+
+leftBtn.addEventListener("click", () => move(false));
+rightBtn.addEventListener("click", () => move(true));
+
+function move(right) {
+    const num = right ? 1 : -1;
+  const current = document.querySelector(".box-selected");
+  const data = current.dataset.box;
+  const nextBox = document.querySelector(
+    `.box[data-box="${Number(data) + num}"]`
+  );
+
+  if (nextBox) {
+    boxes.forEach(box => { 
+        box.classList.remove("box-selected");
+    });
+
+    if (right) {
+        current.classList.add("box-left");
+    } else {
+        current.classList.add("box-right");
+    }
+
+    nextBox.classList.remove("box-right");
+    nextBox.classList.remove("box-left");
+    nextBox.classList.add("box-selected");
+
+    return null;
+  }
+  return null;
+}
